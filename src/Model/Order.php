f65be6c4ff4a9463a7c9671a6d2c6bd800ed205f
huy <huyvo.meete@gmail.com>
2021-04-28 14:37:20 +0700

    Add database model

---
+++ src/Model/Order.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Model;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="place", indexes={@ORM\Index(columns={"business_id"})})
+ */
+class Order
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="bigint")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var int
+     *
+     * @ORM\ManyToOne(targetEntity="User")
+     */
+    private $user;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="created_at", type="bigint")
+     */
+    private $createdAt;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="updated_at", type="bigint")
+     */
+    private $updatedAt;
+}
